3e6b56e773637c536e4ac446a7f44a7e8ec56af6
LOMAN Julius <julius_loman@tempest.sk>
2022-09-25 20:07:42 +0200

    Added support for sending unavailablity events

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # Dynatrace License Metrics
 
-A simple python script to introduce license metrics. It is intended to be executed on hourly basis.
+A simple python script to introduce license metrics and sending events about unmonitored hosts if unmonitored unintentionally. It is intended to be executed on hourly basis by default. 
 
 Following metrics are created:
 
 - `billing.hostunit.consumption`
 
+If OneAgent on a host has UNMONITORED status, script verifies if the agent is set to enabled and sends problem opening events to trigger problem in Dynatace.
+
 # Usage:
 1. Install Python dependencies
 
         pip3 install dynatrace-metric-utils
-        pip3 install dt
+        
+2. Create API token
+API token with following scopes must be created:
+- API v2 - Ingest Metrics
+- API v2 - Read Settings
+- API v2 - Ingest events
+- API v1 - Access problem and event feed, metrics and topology
 
-2. Prepare environment variables
+3. Prepare environment variables
 - `DT_ENVIRONMENT` - URL to Dynatrace environment such as https://xxx123.dynatrace-managed.com/e/18b37a6a-c0bf-4cef-9f76-3ad018986bfe
-- `DT_APITOKEN` - Dynatrace API token with scope of API v2 - Ingest Metrics and API v1 - Access problem and event feed, metrics and topology.
+- `DT_APITOKEN` - Dynatrace API token 
 - `DT_CERTVERIFY` - Set to `true` or `false` to enable/disable cerificate validation or set to path to CA certificate bundle file (PEM)
+- `DT_RELATIVETIME` - Relative time to fetch data back, defaults to **hour**, possible values min, 5mins, 10mins, 15mins, 30mins, hour, 2hours, 6hours, day, 3days, month, week
+- `DT_DRYRUN` - Set to True for a dry-run execution (no data is sent to Dynatrace)
+- `DT_UNMONITORED_EVENTS` - Determines if events about unmonitored hosts which should be monitored will be sent (defaults to True)
+- `DT_UNMONITORED_EVENT_TITLE` - (optional) Event title for unmonitored host
+- `DT_UNMONITORED_EVENT_SEVERITY` - (optional) Event severity for unmonitored host, defaults to AVAILABLITY_EVENT
+- `DT_UNMONITORED_EVENT_TIMEOUT` - (optional) Event timeout in seconds for unmonitored host, defaults to 3600
 
 
 3. Use cron for executing the script on hourly basis
--- licenseHUmetric.py
+++ licenseHUmetric.py
@@ -7,6 +7,7 @@ from dynatrace.metric.utils import (
     MetricError,
     DynatraceMetricsApiConstants,
 )
+
 dtApiToken=os.environ.get("DT_APITOKEN")
 dtEnvironment=os.environ.get("DT_ENVIRONMENT")
 dtCertVerify=os.environ.get("DT_CERTVERIFY")
@@ -18,10 +19,23 @@ if dtCertVerify is not None:
         certVerify=True
     else:
         certVerify=dtCertVerify
+dtRelativeTime=os.environ.get("DT_RELATIVETIME", "hour")
+if (os.environ.get("DT_DRYRUN", "false").lower()=="true"):
+    dtDryRun = True
+else:
+    dtDryRun = False
+if (os.environ.get("DT_UNMONITORED_EVENTS", "true").lower()=="true"):
+    dtUnmonitoredEvents = True
+else: 
+    dtUnmonitoredEvents = False
+dtUnmonitoredEventTitle=os.environ.get("DT_UNMONITORED_EVENT_TITLE","Host is unmonitored")
+dtUnmonitoredEventSeverity=os.environ.get("DT_UNMONITORED_EVENT_SEVERITY", "AVAILABLITY_EVENT")
+dtUnmonitoredEventTimeout=os.environ.get("DT_UNMONITORED_EVENT_TIMEOUT", "3600")
 
 factory = DynatraceMetricsFactory()
 serializer = DynatraceMetricsSerializer(metric_key_prefix="billing",enrich_with_dynatrace_metadata=False)
 consumptionMetrics=[]
+disabledHosts=[]
 
 # Query metric data
 firstQuery = True
@@ -30,7 +44,7 @@ while (firstQuery or pageKey is not None):
     firstQuery = False
     params = {
         "includeDetails": "false",
-        "relativeTime": "hour"
+        "relativeTime": dtRelativeTime
     }
     if pageKey is not None:
         params["nextPageKey"]=pageKey
@@ -39,6 +53,14 @@ while (firstQuery or pageKey is not None):
         jr = response.json()
         pageKey = jr["nextPageKey"]
         for host in jr["hosts"]:
+            # Find disabled hosts
+            try:
+                if host['availabilityState']=="UNMONITORED":
+                    disabledHosts.append(host['hostInfo']['entityId'])
+            except KeyError:
+                pass
+
+            # Provide HU consumption metric value
             dimensions = {}
             try:
                 dimensions["dt.entity.host"]=f"{host['hostInfo']['entityId']}"
@@ -62,10 +84,41 @@ while (firstQuery or pageKey is not None):
 
 # Post metric data
 chunkSize = 500
-for i in range(0,len(consumptionMetrics), chunkSize):    
-    # Uncomment to print metric lines
-    # print("\n".join(consumptionMetrics[i:i+chunkSize]))
-    metricResponse = requests.post(f"{dtEnvironment}/api/v2/metrics/ingest", data="\n".join(consumptionMetrics[i:i+chunkSize]), headers={"Authorization": f"api-token {dtApiToken}"}, verify=certVerify)
-    if metricResponse.status_code!=202 or metricResponse.json()["linesInvalid"]>0:
-        print(metricResponse.status_code)
-        print(metricResponse.text)
+for i in range(0, len(consumptionMetrics), chunkSize):
+    if (dtDryRun):
+        print("\n".join(consumptionMetrics[i:i+chunkSize]))
+    else: 
+        metricResponse = requests.post(f"{dtEnvironment}/api/v2/metrics/ingest", data="\n".join(
+            consumptionMetrics[i:i+chunkSize]), headers={"Authorization": f"api-token {dtApiToken}"}, verify=certVerify)
+        if metricResponse.status_code != 202 or metricResponse.json()["linesInvalid"] > 0:
+            print(metricResponse.status_code)
+            print(metricResponse.text)    
+
+if (dtUnmonitoredEvents):
+    # Check disabled hosts to validate if disabled intentionally and send events
+    for host in disabledHosts:
+        response = requests.get(f"{dtEnvironment}/api/v2/settings/effectiveValues", params={
+                                "schemaIds": "builtin:host.monitoring", "scope": host}, 
+                                headers={"Authorization": f"api-token {dtApiToken}"}, verify=certVerify)
+        if response.status_code == 200:
+            for hostMonitoringConfig in response.json()["items"]:
+                if hostMonitoringConfig['value']['enabled']:
+                    event = {
+                        "eventType": dtUnmonitoredEventSeverity,
+                        "title": dtUnmonitoredEventTitle,
+                        "timeout": dtUnmonitoredEventTimeout,
+                        "entitySelector": f"type(HOST),entityId({host})",
+                        "properties": {
+                            "Actual monitoring state": "UNMONITORED",
+                            "Desired monitoring state": "enabled",
+                            "Desired fullstack mode": hostMonitoringConfig['value']['fullStack'],
+                            "Desired autoinjection mode": hostMonitoringConfig['value']['autoInjection']
+                        }
+                    }
+                    if (dtDryRun):
+                        print(f"Will send event {event}")
+                    else:
+                        response = requests.post(f"{dtEnvironment}/api/v2/events/ingest", json=event, 
+                                headers={"Authorization": f"api-token {dtApiToken}"}, verify=certVerify)
+                        if response.status_code!=201:
+                            print(f"Error while sending event for {host}, {response}")                
